6daf602a05509ed29cb766b71be42ec77dea3260
qing-bu-qin <3499043754@qq.com>
2025-01-22 14:01:34 +0800

    Update 链表__创建链表.cpp

--- 链表__创建链表.cpp
+++ 链表__创建链表.cpp
@@ -1,44 +1,43 @@
+//Linked List: Inserting a node at beginning
 #include <stdio.h>
 #include <stdlib.h>
-
-struct Node* head;
 
 struct Node {
 	int data;
 	struct Node* next;
 };
 
+struct Node* head;
+
+void Insert(int x);
+void Print();
+
+void main() {
+	head = NULL;
+	printf("How many numbers?\n");
+	int n, i,x;
+	scanf_s("%d", &n);
+	for (i = 0; i < n; i++) {
+		printf("Enter the number \n");
+		scanf_s("%d", &x);
+		Insert(x);
+		Print();
+	}
+}
+
+void Insert(int x) {
+	struct Node* temp = (Node*)malloc(sizeof(struct Node));
+	temp->data = x;
+	temp->next = head;
+	head = temp;
+}
+
 void Print() {
-	Node* temp = head;
+	struct Node* temp = head;
+	printf("List is :");
 	while (temp != NULL) {
 		printf("%d ", temp->data);
 		temp = temp->next;
 	}
 	printf("\n");
-}
-
-void Insert(int data, int n) {
-	Node* temp1 = new Node();
-	temp1->data = data;
-	temp1->next = NULL;
-	if (n == 1) {
-		temp1->next = head;
-		head = temp1;
-		return;
-	}
-	Node* temp2 = head;
-	for (int i = 0; i < n - 2; i++) {
-		temp2 = temp2->next;
-	}
-	temp1->next = temp2->next;
-	temp2->next = temp1;
-}
-
-int main() {
-	head = NULL;
-	Insert(2, 1);
-	Insert(3, 2);
-	Insert(4, 1);
-	Insert(5, 2);
-	Print();
 }
